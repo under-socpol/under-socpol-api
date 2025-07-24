0bccf2ae7126fdcf42e836b9dea2b4a807ea0efe
ekyza <ekyza.dev@gmail.com>
2025-07-24 15:56:18 +0700

    increase request body size limit to support rich article content

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,8 @@ const corsOptions = {
   credentials: true,
 };
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(cors(corsOptions));
 
